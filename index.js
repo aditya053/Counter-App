0daa2c8af9e930a1582bbe7376a18c79844a43f8
Aditya053 <dbz29135@gmail.com>
2023-03-23 17:19:15 +0530

    second commit

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 let display=document.querySelector('.counter-display');
-let btns=document.querySelector('.buttons');
+let btns= document.querySelector('.buttons');
+
+btns.addEventListener('click', counter);
+
+const add = (function () {
+    let counter = 0;
+    return function () {counter += 1; return counter;}
+  })();
+
+const sub = (function () {
+    let counter = 0;
+    return function () {counter -= 1; return counter;}
+  })();
+    
+
+let value = 0;
+function counter(e) {
+  let btn = e.target.id;
+  if (btn === 'increment') {
+    value = add();
+  } else if (btn === 'decrement') {
+    value = sub();
+  } else {
+    value = 0;
+  }
+
+    display.textContent = value;
+}
